683ffb121bad86dfb253354c6f1db0338996ed50
Matheus Viana <matheus_o_viana@hotmail.com>
2025-08-26 19:25:27 +0200

    adjusted warning

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3333);
 }
-bootstrap();
+
+void bootstrap();
